0e7d053c920132640511055c21855196c865a8ec
charrus <charlie@rusbridger.com>
2023-07-16 19:09:53 +0100

    Added 3772h script

---
+++ sendsms3772.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import sys
+import time
+
+import requests
+
+from bs4 import BeautifulSoup
+
+host = "192.168.8.1"
+# port = "80"
+port = None
+
+if port:
+    origin = f"http://{host}:{port}"
+else:
+    origin = f"http://{host}"
+
+msg = sys.argv[1]
+
+number = sys.argv[2]
+
+time = time.strftime("%F %T", time.localtime())
+
+payloadxml = (
+    f'<?xml version="1.0" encoding="UTF-8"?>'
+    f"<request>"
+    f"<Index>-1</Index>"
+    f"<Phones><Phone>{number}</Phone></Phones>"
+    f"<Sca></Sca>"
+    f"<Content>{msg}</Content>"
+    f"<Length>{len(msg)}</Length>"
+    f"<Reserved>1</Reserved>"
+    f"<Date>2023-07-16 17:59:55</Date>"
+    f"</request>"
+)
+
+url = f"{origin}/api/sms/send-sms"
+referer = f"{origin}/html/smsinbox.html"
+
+sendsms = requests.get(referer)
+
+soup = BeautifulSoup(sendsms.text, "html.parser")
+for token in soup.findAll('meta', attrs={'name': 'csrf_token'}):
+    token = token['content']
+    break
+
+headers = {
+    "__RequestVerificationToken": token,
+}
+
+r = requests.post(url, headers=headers, data=payloadxml, cookies=sendsms.cookies)
+print(r.text)
